9aa2c9e181bbeadfdc517e26a172cc1c7303e804
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2021-02-03 16:19:49 +0530

    v-1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
-const expressJWT = require('express-jwt');
-const { result } = require('lodash');
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const expressJWT = require("express-jwt");
+// const {result} = require("lodash");
 
 const generateRandomNumber = () => {
 	let k = 4;
-	let result = '';
+	let result = "";
 	while (k-- > 0) {
 		result += Math.floor(Math.random() * 9);
 	}
@@ -15,27 +15,27 @@ const generateRandomNumber = () => {
 };
 
 exports.activationLink = (req, res) => {
-	User.findOne({ email: req.body.email }, (error, result) => {
+	User.findOne({email: req.body.email}, (error, result) => {
 		if (error) {
 			return res.status(400).json({
-				error: 'Error in Registration',
+				error: "Error in Registration",
 			});
 		}
 		if (result) {
 			return res.status(400).json({
-				error: 'User Already exists',
+				error: "User Already exists",
 			});
 		}
 		var transporter = nodemailer.createTransport({
-			service: 'gmail',
+			service: "gmail",
 			auth: {
-				user: 'hari.jsmith494@gmail.com',
-				pass: 'lfpgbeieoqbtzenq',
+				user: "hari.jsmith494@gmail.com",
+				pass: "lfpgbeieoqbtzenq",
 			},
 		});
 		var randomCode = generateRandomNumber();
 		var mailOptions = {
-			from: 'hari.jsmith494@gmail.com',
+			from: "hari.jsmith494@gmail.com",
 			to: req.body.email,
 			subject: `Clothings Verification Mail `,
 			html: `<h2>Your Verification is <i>${randomCode}</i> (expires in 5 minutes) </h2>`,
@@ -43,12 +43,12 @@ exports.activationLink = (req, res) => {
 		transporter.sendMail(mailOptions, function (err, info) {
 			if (err) {
 				return res.status(400).json({
-					error: 'Error in registration',
+					error: "Error in registration",
 				});
 			}
 		});
 		return res.status(200).json({
-			message: 'Check your mail for Activation code ',
+			message: "Check your mail for Activation code ",
 			code: randomCode,
 			time: new Date(),
 		});
@@ -56,10 +56,10 @@ exports.activationLink = (req, res) => {
 };
 
 exports.register = (req, res) => {
-	User.findOne({ email: req.body.email }, (error, result) => {
+	User.findOne({email: req.body.email}, (error, result) => {
 		if (result || error) {
 			return res.status(400).json({
-				error: 'User already Exists',
+				error: "User already Exists",
 			});
 		}
 		bcrypt.hash(req.body.password, 10, function (err, hash) {
@@ -73,11 +73,11 @@ exports.register = (req, res) => {
 			newUser.save((error, result) => {
 				if (error) {
 					return res.status(400).json({
-						error: 'Error in registration',
+						error: "Error in registration",
 					});
 				}
 				return res.status(200).json({
-					message: 'Registration successfull',
+					message: "Registration successfull",
 				});
 			});
 		});
@@ -85,26 +85,26 @@ exports.register = (req, res) => {
 };
 
 exports.login = (req, res) => {
-	User.findOne({ email: req.body.email }, (error, result) => {
+	User.findOne({email: req.body.email}, (error, result) => {
 		if (error || !result) {
 			return res.status(400).json({
-				error: 'user not found',
+				error: "user not found",
 			});
 		}
 		bcrypt.compare(req.body.password, result.password, function (err, check) {
 			if (err || !check) {
 				return res.status(400).json({
-					error: 'Missmatch Password',
+					error: "Missmatch Password",
 				});
 			}
-			let token = jwt.sign({ _id: result._id }, 'secretCode');
-			res.cookie('token', token, {
+			let token = jwt.sign({_id: result._id}, "secretCode");
+			res.cookie("token", token, {
 				expires: new Date(Date.now() + 10),
 			});
 			result.password = undefined;
 			req.user = result;
 			return res.status(200).json({
-				message: 'Successfully Signed in',
+				message: "Successfully Signed in",
 				user: result,
 				token: token,
 			});
@@ -113,23 +113,23 @@ exports.login = (req, res) => {
 };
 
 exports.logout = (req, res) => {
-	res.clearCookie('token');
+	res.clearCookie("token");
 	res.status(200).json({
-		message: 'Sign Out Successfully',
+		message: "Sign Out Successfully",
 	});
 };
 
 exports.isSignedIn = expressJWT({
-	secret: 'secretCode',
-	algorithms: ['HS256'],
-	userProperty: 'auth',
+	secret: "secretCode",
+	algorithms: ["HS256"],
+	userProperty: "auth",
 });
 
 exports.isAuthenticated = (req, res, next) => {
 	let check = req.auth && req.user && req.auth._id == req.user._id;
 	if (!check) {
 		return res.status(400).json({
-			error: 'Unauthorized Access',
+			error: "Unauthorized Access",
 		});
 	}
 	next();
@@ -138,45 +138,70 @@ exports.isAuthenticated = (req, res, next) => {
 exports.isAdmin = (req, res, next) => {
 	if (req.user._id === 0) {
 		res.status(400).json({
-			error: 'Unauthorized Access',
+			error: "Unauthorized Access",
 		});
 	}
 	next();
 };
 
 exports.getOTPforPassword = (req, res) => {
-	User.find({ email: req.body.email }, (error, result) => {
-		if (error || result.length === 0) {
-			return res.status(400).json({
-				error: 'User not Found',
-			});
-		}
-		var transporter = nodemailer.createTransport({
-			service: 'gmail',
-			auth: {
-				user: 'hari.jsmith494@gmail.com',
-				pass: 'lfpgbeieoqbtzenq',
-			},
-		});
-		var OTP = generateRandomNumber();
-		var mailOptions = {
-			from: 'hari.jsmith494@gmail.com',
-			to: req.body.email,
-			subject: `Clothings Verification Mail `,
-			html: `<h2>Your OTP <i>${OTP}</i> (expires in 5 minutes) </h2>`,
-		};
-		transporter.sendMail(mailOptions, function (err, info) {
-			if (err) {
+	console.log(req.body);
+	try {
+		User.find({email: req.body.email}, (error, result) => {
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "application/json");
+
+			if (error || result.length === 0) {
 				return res.status(400).json({
-					error: 'Error in registration',
+					error: "User not Found",
 				});
 			}
+			var transporter = nodemailer.createTransport({
+				service: "gmail",
+				auth: {
+					user: "hari.jsmith494@gmail.com",
+					pass: "fxinrkqjbpsgvclw",
+				},
+			});
+			var OTP = generateRandomNumber();
+			var mailOptions = {
+				from: "hari.jsmith494@gmail.com",
+				to: req.body.email,
+				subject: `Clothings Verification Mail `,
+				html: `<h2>Your OTP <i>${OTP}</i> (expires in 5 minutes) </h2>`,
+			};
+			// let send = async () => {
+			// 	let info = await transporter.sendMail({
+			// 		from: "hari.jsmith494@gmail.com",
+			// 		to: req.body.email,
+			// 		subject: `Clothings Verification Mail `,
+			// 		html: `<h2>Your OTP <i>${OTP}</i> (expires in 5 minutes) </h2>`,
+			// 	});
+			// };
+			transporter.sendMail(mailOptions, function (error, info) {
+				console.log("Ia");
+				if (error) {
+					return res.status(400).json({
+						error: "Error in sending email",
+					});
+				}
+				console.log(OTP, info);
+				return res.status(200).json({
+					OTP: OTP,
+					message: "Check your mail",
+				});
+			});
+			return res.status(200).json({
+				OTP: OTP,
+				message: "Check your mail",
+			});
 		});
-		return res.status(200).json({
-			OTP: OTP,
-			message: 'Check your mail',
+	} catch (error) {
+		console.log(error);
+		return res.status(400).json({
+			error: "Error in sending the otp",
 		});
-	});
+	}
 };
 
 exports.setNewPassword = (req, res) => {
@@ -187,23 +212,20 @@ exports.setNewPassword = (req, res) => {
 			});
 		}
 		User.findOneAndUpdate(
-			{ email: req.body.email },
-			{ $set: { password: hash } },
+			{email: req.body.email},
+			{$set: {password: hash}},
 			(error, result) => {
 				if (error) {
 					return res.status(400).json({
-						error: 'Error in reseting the password',
+						error: "Error in reseting the password",
 					});
 				}
 				return res.status(200).json({
-					message: 'Password updated Successfully',
+					message: "Password updated Successfully",
 				});
 			},
 		);
 	});
 };
 
-exports.reqSellerAccount = (req,res) => {
-
-	
-}
+exports.reqSellerAccount = (req, res) => {};
